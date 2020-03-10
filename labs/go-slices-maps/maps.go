a4372bb113e8ad389d319e98b414e98e1d4168a4
esaupreciado97 <esauplm@gmail.com>
2020-03-10 16:41:39 -0600

    Solved go-slices-maps
    Signed-off-by: esaupreciado97 <esauplm@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+	for _, e := range strings.Fields(s) {
+		m[string(e)] += 1
+	}
+	return m
+}
+
 func main() {
+	wc.Test(WordCount)
 }
