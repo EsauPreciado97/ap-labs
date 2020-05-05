d2fe175c8ba7e1847f7afdcfd34b695682c3957c
esaupreciado97 <esauplm@gmail.com>
2020-05-05 13:40:49 -0500

    Solved Second Partial Challenge
    Signed-off-by: esaupreciado97 <esauplm@gmail.com>

--- challenges/second-partial/chat/client.go
+++ challenges/second-partial/chat/client.go
@@ -1,28 +1,47 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
+// LUIS ESAÚ PRECIADO MARTÍNEZ
+// A01631211
+
 // See page 227.
 
 // Netcat is a simple read/write client for TCP servers.
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 )
 
+var user string
+
 //!+
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8000")
+	if (len(os.Args) < 5) || (os.Args[1] != "-user") || (os.Args[3] != "-server") {
+		log.Fatalf("How to execute: go run client.go -user [user] -server [ip]:[port]\n")
+	}
+
+	user = os.Args[2]
+	server := os.Args[4]
+
+	conn, err := net.Dial("tcp", server)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	_, err = io.WriteString(conn, user+"\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	done := make(chan struct{})
 	go func() {
 		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
-		log.Println("done")
+		fmt.Println("Server was shutdown")
 		done <- struct{}{} // signal the main goroutine
 	}()
 	mustCopy(conn, os.Stdin)
