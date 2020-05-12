ecb43ddf0ddf56879180a4f8cb4040ddcf602d79
esaupreciado97 <esauplm@gmail.com>
2020-05-12 13:49:31 -0500

    Solved File/Dir Lab
    Signed-off-by: esaupreciado97 <esauplm@gmail.com>

--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 #include "logger.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/inotify.h>
+#include <sys/types.h>
 
-int main(){
+#define BUF_LEN sizeof(struct inotify_event) * 1024
+
+int fd;
+int wd;
+int rd;
+char* p;
+struct inotify_event *event;
+
+
+void displayEvent(struct inotify_event* event);
+
+int main(char argc, char** argv)
+{
+    
     // Place your magic here
+    if(argc < 2)
+    {
+        errorf("Invalid input, please type in a directory name\n");
+        return -1;
+    }
+
+    fd = inotify_init1(O_NONBLOCK);
+    wd = inotify_add_watch(fd, argv[1], IN_ALL_EVENTS);
+    char* buffer = (char*)malloc(BUF_LEN);
+
+    while(1)
+    {
+        rd = read(fd, buffer, BUF_LEN);
+        p = buffer;
+        event = (struct inotify_event*)p;
+        for(p = buffer; p < buffer + rd; )
+        {
+            event = (struct inotify_event *) p;
+            displayEvent(event);
+            p += sizeof(struct inotify_event) + event -> len;
+        }
+    }
+    close(fd);
     return 0;
+}
+
+void displayEvent(struct inotify_event* event)
+{
+    if(event->mask & IN_ACCESS)     infof("Directory successfully access\n");
+    if(event->mask & IN_CREATE)     infof("Directory successfully created\n");
+    if(event->mask & IN_DELETE)     errorf("Directory successfully deleted\n");
+    if(event->mask & IN_OPEN)       infof("Open directory\n");
+    if(event->mask & IN_MODIFY)     warnf("Directory successfully modificated\n");
+
 }
